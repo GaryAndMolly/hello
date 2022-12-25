0cb0d00fdbfe261b0ce4e98cc3c3679eb6ba6f6b
gary <runfeng_gai@163.com>
2022-12-25 00:36:40 -0500

    hello

--- src/main/java/com/example/demo/helloController/HelloController.java
+++ src/main/java/com/example/demo/helloController/HelloController.java
@@ -7,6 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String handle01(){
+
         return "Hello, Spring Boot 2!";
+    }
+
+    @RequestMapping("/hi")
+    public String handle02(){
+
+        return "Hello, Spring Boot 3!";
     }
 }
